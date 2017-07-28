a2c12750d15fc8d4048490f52435c54e0fa4913a
Alexey Dvoryanskiy <dvoryanskiy@appbooster.ru>
2017-07-28 17:42:50 +0400

    Work in progress. Implementing rethink db.

--- db/rethink/rethink.go
+++ db/rethink/rethink.go
@@ -64,17 +64,52 @@ func (r *rethink) Create(span *proto.Span) error {
 }
 
 func (r *rethink) Read(traceId string) ([]*proto.Span, error) {
-	rdb.Table("tracing").GetAll(
+	rows, err := rdb.Table("tracing").Filter(rdb.Row.Field("trace_id").Eq(traceId)).Run(r.db)
+	if err != nil {
+		return nil, err
+	}
+
+	spans := []*proto.Span{}
+	if err = rows.All(&spans); err != nil {
+		return nil, err
+	}
+
+	return spans, nil
 }
 
 func (r *rethink) Delete(traceId string) error {
+	if _, err := rdb.Table("tracing").Filter(rdb.Row.Field("trace_id").Eq(traceId)).Delete().RunWrite(r.db); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (r *rethink) Search(name string, limit, offset int64, reverse bool) ([]*proto.Span, error) {
-	return nil, nil
+	query, err := rdb.Table("tracing").Filter(rdb.Row.Field("name").Eq(name)).Skip(offset).Limit(limit)
+	if reverse {
+		query = query.OrderBy(rdb.OrderByOpts{
+			Index: rdb.Asc("timestamp"),
+		})
+	} else {
+		query = query.OrderBy(rdb.OrderByOpts{
+			Index: rdb.Desc("timestamp"),
+		})
+	}
+
+	rows, err = query.Run(r.db)
+	if err != nil {
+		return nil, err
+	}
+
+	spans := []*proto.Span{}
+	if err = rows.All(&spans); err != nil {
+		return nil, err
+	}
+
+	return spans, nil
 }
 
 func (r *rethink) Live(name string, ch chan *proto.Span) error {
-	return nil
+
 }
